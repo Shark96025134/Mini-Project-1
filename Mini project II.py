378488680f11bc16b0c5fc8451ee6742c37b9231
shark9602 <93127662+shark9602@users.noreply.github.com>
2021-11-10 15:45:10 +0530

    Add files via upload

---
+++ Mini project II.py
@@ -0,0 +1,19 @@
+print("Cube and square Calculator!")
+print("Enter your number here:")
+a=int(input())
+print("Enter the operation here:")
+c=(input())
+b=a*a*a
+d=a*a
+if c=="square":
+    print (d)
+if c=="cube":
+    print (b)
+if c=="Square":
+    print (d)
+if c=="Cube":
+    print (b)
+if c=="SQUARE":
+    print (d)
+if c=="CUBE":
+    print (b)
